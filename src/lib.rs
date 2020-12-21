467c8498b1481e93bf1b0e0cc9cbbdc1896191d7
Petros Angelatos <petrosagg@gmail.com>
2020-12-21 11:17:23 +0100

    libs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use itertools::Itertools;
 
 pub fn get_input<F, T>(day: u64, n: u64, func: F) -> Vec<T>
     where F: FnMut(&str) -> T
@@ -22,3 +23,35 @@ pub fn get_input_all<F, T>(day: u64, n: u64, func: F) -> Vec<T>
         .map(func)
         .collect()
 }
+
+pub fn get_input_filter<F, T>(day: u64, n: u64, func: F) -> Vec<T>
+    where F: FnMut(&str) -> Option<T>
+{
+    let path = format!("data/day{}/{}", day, n);
+    fs::read_to_string(&path)
+        .unwrap()
+        .lines()
+        .filter(|l| l.len() > 0)
+        .filter_map(func)
+        .collect()
+}
+
+pub fn get_input_filter_all<F, T>(day: u64, n: u64, func: F) -> Vec<T>
+    where F: FnMut(&str) -> Option<T>
+{
+    let path = format!("data/day{}/{}", day, n);
+    fs::read_to_string(&path)
+        .unwrap()
+        .lines()
+        .filter_map(func)
+        .collect()
+}
+
+pub fn offsets(dimentions: usize) -> impl Iterator<Item=isize> {
+    std::iter::repeat(-1..=1)
+        .take(dimentions)
+        .multi_cartesian_product()
+        .filter(|v| v.iter().any(|e| *e != 0))
+        .flatten()
+}
+
